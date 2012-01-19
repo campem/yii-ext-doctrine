aff2b1e00af19f88cfc47d2c68af02b3a5773abd
Kevin Bradwick <kbradwick@gmail.com>
2012-01-19 20:41:51 +0000

    restructured repo layout

---
+++ tests/Doctrine/Component/CacheComponentTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace KodeFoundry\Test\YiiFramework\Extension\Doctrine\Component;
+
+/**
+ *
+ * @author  Kevin Bradwick <kbradwick@gmail.com>
+ * @package
+ * @license
+ */
+class CacheComponentTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test if there is no configuration then the ArrayCache is implemented
+     */
+    public function testDefaultCacheIsArrayCache()
+    {
+
+    }
+}
